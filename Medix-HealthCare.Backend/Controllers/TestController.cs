dd03ae767eb162aa7e7ae96b74048a3e25bb499c
Siddharth Singh Tiwari <siddharthtiwari9858@gmail.com>
2026-03-06 01:01:23 +0530

    add test controller Medix-backend

---
+++ Medix-HealthCare.Backend/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medix_HealthCare.Backend.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
